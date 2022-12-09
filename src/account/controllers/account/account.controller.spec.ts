826b66f1a29311ad25d41988c9964ec125f6a6dc
mateosofka <mateo.gutierrez@sofka.com.co>
2022-12-09 15:04:05 -0500

    unit tests for account module completed

--- src/account/controllers/account/account.controller.spec.ts
+++ src/account/controllers/account/account.controller.spec.ts
@@ -1,21 +1,102 @@
+import { MovementEntity } from './../../../common/storage/databases/postgres/entities/movement.entity';
+import { AccountDto } from './../../dto/account.to';
+import { AccountEntity } from './../../../common/storage/databases/postgres/entities/account.entity';
+import { ClientService } from './../../../client/services/client/client.service';
 import { TokenGuard } from './../../../common/guards/token.guard';
 import { CanActivate } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountController } from './account.controller';
 import { AccountService } from '../../services/account/account.service';
+import { PaymentDto } from '../../dto/paymnet.dto';
+import { EmailPaymentDto } from '../../dto/email-payment.dto';
 
 describe('AccountController', () => {
   let controller: AccountController;
   let service: AccountService;
+
+  const dateTime = new Date();
+  const accIncome = new AccountEntity();
+  const accOutcome = new AccountEntity();
+  const expectedAccountDto: AccountDto = {
+    balance: 123000000,
+    credit: 50000000,
+    id: 'b146b149-bd56-4877-a518-5abbdac01557',
+    cliId: 'f3689626-8b1d-439c-9696-5bcad7e5fe75',
+    movements: [
+      {
+        id: 'b146b149-bd56-4877-a518-5abbdac01401',
+        accIdIncome: 'b146b149-bd56-4877-a518-5abbdac01557',
+        accIdOutcome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
+        reason: 'this is the reason',
+        amount: 10000,
+        fees: 1,
+        dateTime: dateTime,
+        accIdIncome2: accIncome,
+        accIdOutcome2: accOutcome,
+      },
+      {
+        id: '8fd1a8e3-5147-473f-840f-62981b808a0c',
+        accIdIncome: 'b146b149-bd56-4877-a518-5abbdac01557',
+        accIdOutcome: 'b146b149-bd56-4877-a518-5abbdac01557',
+        reason: 'this is other reason',
+        amount: 10000,
+        fees: 1,
+        dateTime: dateTime,
+        accIdIncome2: accIncome,
+        accIdOutcome2: accOutcome,
+      },
+    ],
+  };
+
+  const paymentDto: PaymentDto = {
+    idIncome: '',
+    idOutcome: '',
+    reason: '',
+    amount: 0,
+  };
+
+  const expectedPayment: MovementEntity = {
+    id: 'b146b149-bd56-4877-a518-5abbdac01401',
+    accIdIncome: 'b146b149-bd56-4877-a518-5abbdac01557',
+    accIdOutcome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
+    reason: 'this is the reason',
+    amount: 10000,
+    fees: 1,
+    dateTime: dateTime,
+    accIdIncome2: accIncome,
+    accIdOutcome2: accOutcome,
+  };
+
+  const expectedLoan: MovementEntity = {
+    id: 'b146b149-bd56-4877-a518-5abbdac01401',
+    accIdIncome: 'b146b149-bd56-4877-a518-5abbdac01557',
+    accIdOutcome: 'b146b149-bd56-4877-a518-5abbdac01557',
+    reason: 'Loan',
+    amount: 10000,
+    fees: 60,
+    dateTime: dateTime,
+    accIdIncome2: accIncome,
+    accIdOutcome2: accOutcome,
+  };
+
   beforeEach(async () => {
     const mockTokenGuard: CanActivate = { canActivate: jest.fn(() => true) };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AccountController],
       providers: [
-        AccountService,
         {
           provide: AccountService,
-          useValue: {},
+          useValue: {
+            getAccountInfo: jest.fn().mockResolvedValue(expectedAccountDto),
+            newLoan: jest.fn().mockResolvedValue(expectedLoan),
+            newPayment: jest.fn().mockResolvedValue(expectedPayment),
+          },
+        },
+        {
+          provide: ClientService,
+          useValue: {
+            findByEmail: jest.fn().mockResolvedValue(paymentDto),
+          },
         },
       ],
     })
@@ -35,37 +116,10 @@ describe('AccountController', () => {
   it('should return account balance and the last movements of client', async () => {
     //Arrange
     const id = 'f3689626-8b1d-439c-9696-5bcad7e5fe75';
-    const expected = {
-      balance: 123000000,
-      credit: 50000000,
-      id: 'b146b149-bd56-4877-a518-5abbdac01557',
-      cliId: 'f3689626-8b1d-439c-9696-5bcad7e5fe75',
-      movements: [
-        {
-          id: 'b146b149-bd56-4877-a518-5abbdac01557',
-          idIncome: 'f3689626-8b1d-439c-9696-5bcad7e5fe75',
-          idOutcome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
-          reason: 'this is the reason',
-          amount: 10000,
-          fees: 1,
-          dateTime: '22/06/2022 13:55',
-        },
-        {
-          id: '8fd1a8e3-5147-473f-840f-62981b808a0c',
-          idIncome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
-          idOutcome: 'f3689626-8b1d-439c-9696-5bcad7e5fe75',
-          reason: 'this is other reason',
-          amount: 10000,
-          fees: 1,
-          dateTime: '22/10/2022 13:55',
-        },
-      ],
-    };
     //Act
     const result = await controller.getAccount(id);
     //Assert
-    expect(service.getAccountInfo(id)).toHaveBeenCalled();
-    expect(result).toEqual(expected);
+    expect(result).toEqual(expectedAccountDto);
   });
 
   it('should process de Loan requirement and return the movement', async () => {
@@ -74,44 +128,23 @@ describe('AccountController', () => {
       idIncome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
       amount: 10,
     };
-    const expected = {
-      id: 'b146b149-bd56-4877-a518-5abbdac01557',
-      idIncome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
-      idOutcome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
-      reason: 'Loan',
-      amount: 10,
-      fees: 60,
-      dateTime: '22/06/2022 13:55',
-    };
     //Act
-    const result = await controller.loan(dto);
+    const result = await controller.createLoan(dto);
     //Assert
-    expect(service.newLoan(dto)).toHaveBeenCalled();
-    expect(result).toEqual(expected);
+    expect(result).toEqual(expectedLoan);
   });
 
   it('should process a payment and return the new movement', async () => {
     //Arrange
-    const dto = {
-      emailIncome: 'income@mail.com',
-      idOutcome: 'f3689626-8b1d-439c-9696-5bcad7e5fe75',
-      reason: 'Payment',
-      amount: 10,
+    const dto: EmailPaymentDto = {
+      emailIncome: '',
+      idOutcome: '',
+      reason: '',
+      amount: 0,
     };
-    const expected = {
-      id: 'b146b149-bd56-4877-a518-5abbdac01557',
-      idIncome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
-      idOutcome: 'f3689626-8b1d-439c-9696-5bcad7e5fe75',
-      reason: 'Payment',
-      amount: 10,
-      fees: 1,
-      dateTime: '22/06/2022 13:55',
-    };
-
     //Act
-    const result = await service.payment(dto);
+    const result = await controller.createPayment(dto);
     //Assert
-    expect(service.newPayment(dto)).toHaveBeenCalled();
-    expect(result).toEqual(expected);
+    expect(result).toEqual(expectedPayment);
   });
 });
--- src/account/services/account/account.service.spec.ts
+++ src/account/services/account/account.service.spec.ts
@@ -1,12 +1,130 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountService } from './account.service';
+import { AccountEntity } from '../../../common/storage/databases/postgres/entities/account.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MovementsService } from '../movements/movements.service';
+import { AccountDto } from '../../dto/account.to';
+import { ClientEntity } from '../../../common/storage/databases/postgres/entities/client.entity';
+import { MovementEntity } from '../../../common/storage/databases/postgres/entities/movement.entity';
 
 describe('AccountService', () => {
   let service: AccountService;
+  const dateTime = new Date();
+  const accIncome = new AccountEntity();
+  const accOutcome = new AccountEntity();
+  const expectedAccountDto: AccountDto = {
+    balance: 123000000,
+    credit: 50000000,
+    id: 'b146b149-bd56-4877-a518-5abbdac01557',
+    cliId: 'f3689626-8b1d-439c-9696-5bcad7e5fe75',
+    movements: [
+      {
+        id: 'b146b149-bd56-4877-a518-5abbdac01401',
+        accIdIncome: 'b146b149-bd56-4877-a518-5abbdac01557',
+        accIdOutcome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
+        reason: 'this is the reason',
+        amount: 10000,
+        fees: 1,
+        dateTime: dateTime,
+        accIdIncome2: accIncome,
+        accIdOutcome2: accOutcome,
+      },
+      {
+        id: '8fd1a8e3-5147-473f-840f-62981b808a0c',
+        accIdIncome: 'b146b149-bd56-4877-a518-5abbdac01557',
+        accIdOutcome: 'b146b149-bd56-4877-a518-5abbdac01557',
+        reason: 'this is other reason',
+        amount: 10000,
+        fees: 1,
+        dateTime: dateTime,
+        accIdIncome2: accIncome,
+        accIdOutcome2: accOutcome,
+      },
+    ],
+  };
 
+  const returnedAccount: AccountEntity = {
+    balance: 123000000,
+    credit: 50000000,
+    id: 'b146b149-bd56-4877-a518-5abbdac01557',
+    cliId: 'f3689626-8b1d-439c-9696-5bcad7e5fe75',
+    movements: [],
+    state: 0,
+    accCreatedAt: undefined,
+    accUpdatedAt: undefined,
+    accDeletedAt: undefined,
+    cli: new ClientEntity(),
+    movements2: [],
+  };
+
+  const movements: MovementEntity[] = [
+    {
+      id: 'b146b149-bd56-4877-a518-5abbdac01401',
+      accIdIncome: 'b146b149-bd56-4877-a518-5abbdac01557',
+      accIdOutcome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
+      reason: 'this is the reason',
+      amount: 10000,
+      fees: 1,
+      dateTime: dateTime,
+      accIdIncome2: accIncome,
+      accIdOutcome2: accOutcome,
+    },
+    {
+      id: '8fd1a8e3-5147-473f-840f-62981b808a0c',
+      accIdIncome: 'b146b149-bd56-4877-a518-5abbdac01557',
+      accIdOutcome: 'b146b149-bd56-4877-a518-5abbdac01557',
+      reason: 'this is other reason',
+      amount: 10000,
+      fees: 1,
+      dateTime: dateTime,
+      accIdIncome2: accIncome,
+      accIdOutcome2: accOutcome,
+    },
+  ];
+
+  const expectedPayment: MovementEntity = {
+    id: 'b146b149-bd56-4877-a518-5abbdac01401',
+    accIdIncome: 'b146b149-bd56-4877-a518-5abbdac01557',
+    accIdOutcome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
+    reason: 'this is the reason',
+    amount: 10000,
+    fees: 1,
+    dateTime: dateTime,
+    accIdIncome2: accIncome,
+    accIdOutcome2: accOutcome,
+  };
+
+  const expectedLoan: MovementEntity = {
+    id: 'b146b149-bd56-4877-a518-5abbdac01401',
+    accIdIncome: 'b146b149-bd56-4877-a518-5abbdac01557',
+    accIdOutcome: 'b146b149-bd56-4877-a518-5abbdac01557',
+    reason: 'Loan',
+    amount: 10000,
+    fees: 60,
+    dateTime: dateTime,
+    accIdIncome2: accIncome,
+    accIdOutcome2: accOutcome,
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AccountService],
+      providers: [
+        AccountService,
+        {
+          provide: getRepositoryToken(AccountEntity),
+          useValue: {
+            save: jest.fn(),
+            findOneOrFail: jest.fn().mockResolvedValue(returnedAccount),
+          },
+        },
+        {
+          provide: MovementsService,
+          useValue: {
+            getMovments: jest.fn().mockResolvedValue(movements),
+            addLoan: jest.fn().mockResolvedValue(expectedLoan),
+            addPayment: jest.fn().mockResolvedValue(expectedPayment),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AccountService>(AccountService);
@@ -19,36 +137,11 @@ describe('AccountService', () => {
   it('should return account balance and the last movements of client by the id', async () => {
     //Arrange
     const id = 'f3689626-8b1d-439c-9696-5bcad7e5fe75';
-    const expected = {
-      balance: 123000000,
-      credit: 50000000,
-      id: 'b146b149-bd56-4877-a518-5abbdac01557',
-      cliId: 'f3689626-8b1d-439c-9696-5bcad7e5fe75',
-      movements: [
-        {
-          id: 'b146b149-bd56-4877-a518-5abbdac01557',
-          idIncome: 'f3689626-8b1d-439c-9696-5bcad7e5fe75',
-          idOutcome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
-          reason: 'this is the reason',
-          amount: 10000,
-          fees: 1,
-          dateTime: '22/06/2022 13:55',
-        },
-        {
-          id: '8fd1a8e3-5147-473f-840f-62981b808a0c',
-          idIncome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
-          idOutcome: 'f3689626-8b1d-439c-9696-5bcad7e5fe75',
-          reason: 'this is other reason',
-          amount: 10000,
-          fees: 1,
-          dateTime: '22/10/2022 13:55',
-        },
-      ],
-    };
+
     //Act
-    const result = await service.getAccountInfo(id);
+    const result: AccountDto = await service.getAccountInfo(id);
     //Assert
-    expect(result).toEqual(expected);
+    expect(result).toEqual(expectedAccountDto);
   });
 
   it('should add to balance and substract from the credit of a account due to a Loan', async () => {
@@ -57,19 +150,10 @@ describe('AccountService', () => {
       idIncome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
       amount: 60,
     };
-    const expected = {
-      id: 'b146b149-bd56-4877-a518-5abbdac01557',
-      idIncome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
-      idOutcome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
-      reason: 'Loan',
-      amount: 10,
-      fees: 60,
-      dateTime: '22/06/2022 13:55',
-    };
     //Act
     const result = await service.newLoan(dto);
     //Assert
-    expect(result).toEqual(expected);
+    expect(result).toEqual(expectedLoan);
   });
 
   it('should edit de balance of the account involved in a Payment', async () => {
@@ -80,19 +164,10 @@ describe('AccountService', () => {
       reason: 'Payment',
       amount: 10,
     };
-    const expected = {
-      id: 'b146b149-bd56-4877-a518-5abbdac01557',
-      idIncome: '1ddc64c0-56c0-40e0-83ee-16da62a4042f',
-      idOutcome: 'f3689626-8b1d-439c-9696-5bcad7e5fe75',
-      reason: 'Payment',
-      amount: 10,
-      fees: 1,
-      dateTime: '22/06/2022 13:55',
-    };
 
     //Act
     const result = await service.newPayment(dto);
     //Assert
-    expect(result).toEqual(expected);
+    expect(result).toEqual(expectedPayment);
   });
 });
